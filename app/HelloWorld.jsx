542b16b3852c294d0f1417d85c72b075c72d82eb
May Thet Naing <maythetnaing@blueinternet-mm.com>
2017-07-03 12:05:42 +0630

    Add new files

---
+++ app/HelloWorld.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <p>Hello Ma May Thet</p>
+         </div>
+      );
+   }
+}
+
+export default HelloWorld;
---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld.jsx';
+ 
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
+/*
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+
+class HelloWorld extends React.Component { 
+  render() {
+    return ( Hello, React! ) 
+  } 
+};
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+*/
